e7130f70ef1e8209901221d400527d93a1f34852
carsin <carsonfreedman1@gmail.com>
2020-04-26 12:07:39 -1000

    unminify veritas

--- public/javascripts/veritas.js
+++ public/javascripts/veritas.js
@@ -1,1 +1,170 @@
-var isFixed;$(".smooth-scroll").click(function(n){var o=$(this).attr("href");$("html, body").stop().animate({scrollTop:$(o).offset().top-navHeight+1},300),n.preventDefault()});var lastId,clicked=!1;function hideNav(){$(window).width()<=800&&(toggleNavElements(),removeNavDropdownLinks())}function toggleNavElements(){$(".mobile-toggle").toggleClass("mobile-toggled"),$(".nav-items").toggleClass("nav-appear"),$(".nav").toggleClass("nav-toggled"),$("body").toggleClass("no-scroll"),isFixed||$(".nav").toggleClass("fixed")}function addNavDropdownLinks(){$(".nav-dropdown-item ul li").each(function(){$(".nav-items").append("<div class='nav-item nav-appended-dropdown'>"+$(this).html()+"</div>")})}function removeNavDropdownLinks(){$(".nav-appended-dropdown").each(function(){$(this).remove()})}$(document).ready(function(){isFixed=$(".nav").hasClass("fixed"),$(".mobile-toggle").click(function(){toggleNavElements(),$(".mobile-toggle").hasClass("mobile-toggled")?addNavDropdownLinks():removeNavDropdownLinks(),clicked||($(".nav-appear .nav-item a").click(hideNav),clicked=!0)}),scrollEvent()});var nav=$(".nav"),navHeight=nav.outerHeight(),navItems=$(".nav").find(".nav-item a"),scrollItems=navItems.map(function(){var n=$($(this).attr("href"));if(n.length)return n});function scrollEvent(){if(nav.hasClass("nav-scrollspy")||nav.hasClass("nav-background-adapt")){var n=$(this).scrollTop()+navHeight+1,o=scrollItems.map(function(){if($(this).offset().top<n)return this}),a=(o=o[o.length-1])&&o.length?o[0].id:"";if(nav.hasClass("nav-scrollspy")&&lastId!==a&&(lastId=a,navItems.parent().removeClass("active").end().filter("[href='#"+a+"']").parent().addClass("active")),nav.hasClass("nav-background-adapt")&&nav.css("background-color")!==$("#"+a).css("background-color")){var e=$("#"+a).css("background-color");!0!==nav.data("nav-allow-adaptive-transparency")?("rgba(0, 0, 0, 0)"===e&&(e=$("body").css("background-color")),nav.css("background-color",e),$(".nav-items").css("background-color",e),$(".nav-items").find(".nav-dropdown-item ul").css("background-color",e),setTimeout(function(){var n=nav.css("background-color"),o=n.substring(n.indexOf("(")+1,n.lastIndexOf(")")).split(/,\s*/);navItems.css("color",determineTextColor(o[0],o[1],o[2])),$(".nav").find(".nav-dropdown-title").css("color",determineTextColor(o[0],o[1],o[2])),$(".nav").find(".nav-dropdown-item ul li a").css("color",determineTextColor(o[0],o[1],o[2])),$(".nav").find(".toggle-line").css("background-color",determineTextColor(o[0],o[1],o[2]))},90)):(nav.css("background-color",e),$(".nav-items").css("background-color",e),$(".nav-items").find(".nav-dropdown-item ul").css("background-color",e),"rgba(0, 0, 0, 0)"===e?($("#nav-adaptive-icon").attr("src","websrc/img/logo-white.png"),navItems.css("color","white"),$(".nav").find(".nav-dropdown-title").css("color","white"),$(".nav").find(".nav-dropdown-item ul li a").css("color","white"),$(".nav").find(".toggle-line").css("background-color","white")):setTimeout(function(){var n=nav.css("background-color"),o=n.substring(n.indexOf("(")+1,n.lastIndexOf(")")).split(/,\s*/),a=determineTextColor(o[0],o[1],o[2]);navItems.css("color",a),$(".nav").find(".nav-dropdown-title").css("color",a),$(".nav").find(".nav-dropdown-item ul li a").css("color",a),$(".nav").find(".toggle-line").css("background-color",a),"black"===a?$("#nav-adaptive-icon").attr("src","websrc/img/logos.png"):$("#nav-adaptive-icon").attr("src","websrc/img/logo-white.png")},90))}}}function determineTextColor(n,o,a){for(var e=[n/255,o/255,a/255],s=0;s<e.length;s++)e[s]<=.03928?e[s]=e[s]/12.92:e[s]=Math.pow((e[s]+.055)/1.055,2.4);return.2126*e[0]+.7152*e[1]+.0722*e[2]>.179?"black":"white"}$(window).scroll(scrollEvent);
+/*
+  ----------[ Table of Contents ]----------
+   > 1. Smooth scroll
+   > 2. Navigation Bar
+  -----------------------------------------
+*/
+
+// -----------[ 1. Smooth Scroll ]----------
+
+// Overrides default page scroll functionality & instead uses smooth scroll
+$(".smooth-scroll").click(function(e) {
+    var href = $(this).attr("href");
+    $('html, body').stop().animate({
+        scrollTop: $(href).offset().top - navHeight + 1
+    }, 300);
+    e.preventDefault();
+});
+
+
+// -----------[ 2. Navigation Bar ]----------
+var isFixed;
+var clicked = false;
+
+$(document).ready(function() {
+    isFixed = $(".nav").hasClass("fixed");
+    // Show nav on click
+    $(".mobile-toggle").click(function() {
+        toggleNavElements();
+        if ($(".mobile-toggle").hasClass("mobile-toggled")) {
+            addNavDropdownLinks();
+        } else {
+            removeNavDropdownLinks();
+        }
+
+        if (!clicked) {
+            $(".nav-appear .nav-item a").click(hideNav);
+            clicked = true;
+        }
+    });
+    scrollEvent();
+});
+
+// Remove navbar when clicking on a link
+function hideNav() {
+	if ($(window).width() <= 800){
+  	    toggleNavElements();
+        removeNavDropdownLinks();
+    }
+}
+
+// Show mobile full screen nav
+function toggleNavElements() {
+    $(".mobile-toggle").toggleClass("mobile-toggled");
+    $(".nav-items").toggleClass("nav-appear");
+    $(".nav").toggleClass("nav-toggled");
+    $("body").toggleClass("no-scroll");
+    if (!isFixed) { $(".nav").toggleClass("fixed"); }
+}
+
+// Add links from dropdown menus into regular mobile nav
+function addNavDropdownLinks() {
+    $(".nav-dropdown-item ul li").each(function() {
+        $(".nav-items").append("<div class='nav-item nav-appended-dropdown'>" + $(this).html() + "</div>");
+    });
+}
+
+// Remove Dropdown links from regular mobile nav
+function removeNavDropdownLinks() {
+    $(".nav-appended-dropdown").each(function() {
+        $(this).remove();
+    });
+}
+
+// Scrollspy variables
+var lastId;
+var nav = $(".nav");
+var navHeight = nav.outerHeight();
+var navItems = $(".nav").find(".nav-item a");
+var scrollItems = navItems.map(function() {
+    var item = $($(this).attr("href"));
+    if (item.length) { return item; }
+});
+
+// Scroll event
+$(window).scroll(scrollEvent);
+
+function scrollEvent() {
+    if (nav.hasClass("nav-scrollspy") || nav.hasClass("nav-background-adapt")) {
+        // Get container scroll position
+        var fromTop = $(this).scrollTop() + navHeight + 1;
+
+        // Get id of current scroll item
+        var cur = scrollItems.map(function() {
+            if ($(this).offset().top < fromTop)
+            return this;
+        });
+
+        // Get the id of the current element
+        cur = cur[cur.length-1];
+        var id = cur && cur.length ? cur[0].id : "";
+
+        if (nav.hasClass("nav-scrollspy")) {
+            if (lastId !== id) {
+                lastId = id;
+                // Set / remove active class
+                navItems.parent().removeClass("active").end().filter("[href='#" + id + "']").parent().addClass("active");
+            }
+        }
+
+        if (nav.hasClass("nav-background-adapt")) {
+            if (nav.css("background-color") !== $("#" + id).css("background-color")) {
+                var itemColor = $("#" + id).css("background-color");
+
+                // If item is transparent, fuck that shit. Use body as fallback color unless nav has nav-allow-adaptive-transparency data attribute.
+                if (nav.data("nav-allow-adaptive-transparency") !== true) {
+                    if (itemColor === "rgba(0, 0, 0, 0)") { itemColor = $("body").css("background-color"); }
+                    // Set background color of nav & nav items to current section's background
+                    nav.css("background-color", itemColor);
+                    $(".nav-items").css("background-color", itemColor);
+                    $(".nav-items").find(".nav-dropdown-item ul").css("background-color", itemColor);
+                    setTimeout(function() {
+                        var navColor = nav.css("background-color");
+                        var navColorsOnly = navColor.substring(navColor.indexOf('(') + 1, navColor.lastIndexOf(')')).split(/,\s*/);
+                        navItems.css("color", determineTextColor(navColorsOnly[0], navColorsOnly[1], navColorsOnly[2]));
+                        $(".nav").find(".nav-dropdown-title").css("color", determineTextColor(navColorsOnly[0], navColorsOnly[1], navColorsOnly[2]));
+                        $(".nav").find(".nav-dropdown-item ul li a").css("color", determineTextColor(navColorsOnly[0], navColorsOnly[1], navColorsOnly[2]));
+                        $(".nav").find(".toggle-line").css("background-color", determineTextColor(navColorsOnly[0], navColorsOnly[1], navColorsOnly[2]));
+                    }, 90);
+                } else {
+                    nav.css("background-color", itemColor);
+                    $(".nav-items").css("background-color", itemColor);
+                    $(".nav-items").find(".nav-dropdown-item ul").css("background-color", itemColor);
+                    if (itemColor === "rgba(0, 0, 0, 0)") {
+                        $("#nav-adaptive-icon").attr("src", "websrc/img/logo-white.png");
+                        navItems.css("color", "white");
+                        $(".nav").find(".nav-dropdown-title").css("color", "white");
+                        $(".nav").find(".nav-dropdown-item ul li a").css("color", "white");
+                        $(".nav").find(".toggle-line").css("background-color", "white");
+                    } else {
+                        setTimeout(function() {
+                            var navColor = nav.css("background-color");
+                            var navColorsOnly = navColor.substring(navColor.indexOf('(') + 1, navColor.lastIndexOf(')')).split(/,\s*/);
+                            var newColor = determineTextColor(navColorsOnly[0], navColorsOnly[1], navColorsOnly[2]);
+                            navItems.css("color", newColor);
+                            $(".nav").find(".nav-dropdown-title").css("color", newColor);
+                            $(".nav").find(".nav-dropdown-item ul li a").css("color", newColor);
+                            $(".nav").find(".toggle-line").css("background-color", newColor);
+                            newColor === ("black") ? $("#nav-adaptive-icon").attr("src", "websrc/img/logos.png") : $("#nav-adaptive-icon").attr("src", "websrc/img/logo-white.png");
+                        }, 90);
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Determines whether color should be black or white based on input rgb
+function determineTextColor(red, green, blue) {
+    var c = [red/255, green/255, blue/255];
+
+    for (var i = 0; i < c.length; i++) {
+        c[i] <= 0.03928 ?  c[i] = c[i] / 12.92 : c[i] = Math.pow((c[i] + 0.055) / 1.055, 2.4);
+    }
+
+    if (0.2126 * c[0] + 0.7152 * c[1] + 0.0722 * c[2] > 0.179) {
+        return "black";
+    } else {
+        return "white";
+    }
+}
