fe8aba77c8388886a35971ad17acf2b6866554ae
carsin <carsonfreedman1@gmail.com>
2020-04-26 13:28:38 -1000

    add clientside javascrippt

--- controllers/landing.js
+++ controllers/landing.js
@@ -5,7 +5,6 @@ exports.renderLanding= function(req, res, next) {
 exports.submitUsername= function(req, res, next) {
     if (req.body.usernameInput.length == 0) {
         res.send("username too short");
-        res.redirect("/");
     }
 
     console.log("username: ", req.body.usernameInput)
