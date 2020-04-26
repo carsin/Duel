f600ee9c6958b9b1d937afb01199f7945a307ea9
carsin <carsonfreedman1@gmail.com>
2020-04-25 16:25:04 -1000

    add username thing

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,3 +1,8 @@
-exports.getLanding= function(req, res, next) {
+exports.renderLanding= function(req, res, next) {
     res.render("landing", { title: "Express" });
+}
+
+exports.submitUsername= function(req, res, next) {
+    console.log("username: ", req.body.usernameInput)
+    res.redirect("/");
 }
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 var landing = require("../controllers/landing");
 
 /* GET home page. */
-router.get("/", landing.getLanding);
+router.get("/", landing.renderLanding);
+
+router.post("/", landing.submitUsername);
 
 module.exports = router;
