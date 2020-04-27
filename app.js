91c8ec9a4035672bca818afb8b6799cadfc0cdfb
carsin <carsonfreedman1@gmail.com>
2020-04-26 17:06:14 -1000

    my first route

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var app = express();
 
-app.get("/", function(req, res) {
-    res.send("Hello world!")
-});
+var indexRouter = require("./routes/index.js");
+
+app.use("/", indexRouter);
 
 var server = app.listen("3000", function() {
     console.log("Server started on port 3000");
