4293f3d2bc80cdaa7b64bfe6b4ce9af7b86674b7
carsin <carsonfreedman1@gmail.com>
2020-04-26 19:27:00 -1000

    add id generation and single game selection

--- routes/create.js
+++ routes/create.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var hri = require("human-readable-ids").hri;
 
 router.get("/", function(req, res, next) {
     res.render("create", {
@@ -8,7 +9,8 @@ router.get("/", function(req, res, next) {
 });
 
 router.post("/", function(req, res, next) {
-    console.log("game created by " + req.body.usernameInput);
+    console.log(req.body.selectedGame + " game created by " + req.body.usernameInput);
+    console.log("generating game with ID: " + hri.random());
     res.redirect("/");
 });
 
