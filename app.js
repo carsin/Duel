bf11aa2a9092e67c42ee00c1fe1a9ceaf841262d
carsin <carsonfreedman1@gmail.com>
2020-04-26 17:24:18 -1000

    general: update

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const logger = require("morgan");
+const app = express();
 
-var indexRouter = require("./routes/index.js");
+app.use(logger("dev"));
+app.use(express.static("public"));
+
+const indexRouter = require("./routes/index.js");
 
 app.use("/", indexRouter);
 
-var server = app.listen("3000", function() {
-    console.log("Server started on port 3000");
+var server = app.listen(3000, function() {
+    var port = server.address().port
+
+    console.log("Server running on localhost:" + port)
 });
